ee6cf5671fa0826a8acaf97d592b160caab16099
Anthony Sung <anthonysung@MacBook-Pro-3.local>
2025-04-19 19:14:07 +0800

    renew

--- export_data.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-if __name__ == "__main__":
-    RAW_DIR = "./data/raw"
-
-    TW_US_export_HS = pd.read_csv(os.path.join(RAW_DIR, "TW_US_Export_Ranking.csv"))
-    print(TW_US_export_HS)
